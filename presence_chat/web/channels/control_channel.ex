a0a801125134c75fb6e0d37fa8399f02da2de2b6
Josh Adams <josh.rubyist@gmail.com>
2016-08-02 20:37:20 -0500

    Get rid of useless bits in ControlChannel

--- presence_chat/web/channels/control_channel.ex
+++ presence_chat/web/channels/control_channel.ex
@@ -1,8 +1,6 @@
 defmodule PresenceChat.ControlChannel do
   use PresenceChat.Web, :channel
   alias PresenceChat.Presence
-  require Logger
-  @two_way_regex ~r/<->/
 
   def join("control:" <> username, msg, socket) do
     {:ok, socket}
